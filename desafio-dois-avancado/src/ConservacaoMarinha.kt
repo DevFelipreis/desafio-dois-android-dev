70794268f5af85c355ec33b8e1da3c0d5597ecc9
DevFelipreis <lfreis.contato@gmail.com>
2024-08-27 20:19:01 -0300

    feat: class ConservacaoMarinha

--- desafio-dois-avancado/src/ConservacaoMarinha.kt
+++ desafio-dois-avancado/src/ConservacaoMarinha.kt
@@ -1,5 +1,44 @@
 package ConservacaoMarinha
 
-class ConservacaoMarinha(){
+abstract class ConservacaoMarinha(
+    val tipoAmbiente: String,
+    val areaProtegidaEmKm2: Double,
+    val possuiProgramaMonitoramento: Boolean
+) {
+    abstract fun calcularEficienciaConservacao(): ConservacaoMarinha
 
+    abstract fun exibirInformacoes(): String
+}
+
+class ReservaMarinha(
+    tipoAmbiente: String,
+    areaProtegidaEmKm2: Double,
+    possuiProgramaMonitoramento: Boolean,
+    val tipoReserva: String,
+    var eficiencia: Double = 0.0
+) : ConservacaoMarinha(tipoAmbiente, areaProtegidaEmKm2, possuiProgramaMonitoramento) {
+
+    override fun calcularEficienciaConservacao(): ConservacaoMarinha {
+        eficiencia = if (possuiProgramaMonitoramento) {
+            areaProtegidaEmKm2 * 0.9
+        } else {
+            areaProtegidaEmKm2 * 0.5
+        }
+
+        return this
+    }
+
+    override fun exibirInformacoes(): String {
+        calcularEficienciaConservacao()
+
+        val monitoramento: String = if (possuiProgramaMonitoramento) "SIM" else "NÃO"
+
+        return """
+            Tipo de Ambiente Marinho: $tipoAmbiente
+            Área Protegida: $areaProtegidaEmKm2 km²
+            Possui Programa de Monitoramento: $monitoramento
+            Tipo de Reserva: $tipoReserva
+            Eficiência de Conservação: $eficiencia
+        """.trimIndent()
+    }
 }
--- desafio-dois-avancado/src/Main.kt
+++ desafio-dois-avancado/src/Main.kt
@@ -1,5 +1,20 @@
-import ConservacaoMarinha.ConservacaoMarinha
+import ConservacaoMarinha.ReservaMarinha
 
-fun main(){
-    println("Hello World")
+fun main() {
+    println("Digite o tipo de ambiente(Zona Litorânea, Zona Nerítica," +
+            "Zona Abatial ou Zona Abissal):")
+    val tipoAmbiente = readln().toString()
+
+    println("Digite a área total protegida:")
+    val areaProtegidaEmKm2 = readln().toDouble()
+
+    println("Essa área tem monitoramento? (SIM ou NÃO):")
+    val possuiProgramaMonitoramento = readln().toString().toLowerCase() == "sim"
+
+    println("Digite o tipo de reserva(Estações Ecológicas, Reservas Biológicas, Parques Nacionais," +
+            "Monumentos Naturais ou Refúgios da Vida Silvestre:")
+    val tipoReserva = readln().toString()
+
+    val reserva = ReservaMarinha(tipoAmbiente, areaProtegidaEmKm2, possuiProgramaMonitoramento, tipoReserva)
+    println(reserva.exibirInformacoes())
 }
