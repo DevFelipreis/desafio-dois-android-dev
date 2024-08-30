9dbfbd94026911f18038472fadc17797425e556f
marevandro <marevandro95@gmail.com>
2024-08-30 18:46:09 -0300

    Desafio dois, nivel iniciante, iniciado

--- desafio-dois-iniciante/src/Main.kt
+++ desafio-dois-iniciante/src/Main.kt
@@ -1,3 +1,29 @@
-fun main(){
-    println("Hello World")
+class  ConservacaoMarinha(
+    var tipoAmbiente: String,
+    var areaProtegidaEmKm2: Double,
+    var possuiProgramaMonitoramento: Boolean,
+){
+    init{
+        println("Tipo de Ambiente: $tipoAmbiente")
+        println("Área Protegida: $areaProtegidaEmKm2 km²")
+        println("Possui Monitoramento: $possuiProgramaMonitoramento")
+    }
+    var ambiente: String = tipoAmbiente
+        get() = field
+        set(value) {
+            field = value
+            println("Novo tipo de Ambiente: $field")
+        }
+
+    //Isabel - Entender qual a função do constructor secundário:
+    constructor(tipoAmbiente: String): this(tipoAmbiente, 0.0, false){
+
+    }
+}
+
+fun main() {
+    //Isabel - Realizar os inputs da tarefa
+    val conservacao: ConservacaoMarinha = ConservacaoMarinha("Coral",100.0, true)
+    conservacao.ambiente = "Manguezal"
+
 }
