bc15aeb07d2ca89f60162df6279903a9cadf7320
Márcio Evandro <62778708+marevandro@users.noreply.github.com>
2024-08-31 16:29:19 -0300

    Update Main.kt
    Pequeno ajuste relacionado ao Get

--- desafio-dois-iniciante/src/Main.kt
+++ desafio-dois-iniciante/src/Main.kt
@@ -3,29 +3,32 @@ class  ConservacaoMarinha(
     var areaProtegidaEmKm2: Double,
     var possuiProgramaMonitoramento: Boolean,
 ){
-    init{
-        println("Tipo de Ambiente: $tipoAmbiente")
-        println("Área Protegida: $areaProtegidaEmKm2 km²")
-        println("Possui Monitoramento: $possuiProgramaMonitoramento")
-    }
-    var ambiente: String = tipoAmbiente
-        get() = field
+    var parqueNacional = ""
+        get(){
+            println("Parque Nacional Marinho dos Abrolhos")
+            return field
+        }
         set(value) {
             field = value
             println("Novo tipo de Ambiente: $field")
         }
+    init{
+        print("\n")
+        println("Tipo de Ambiente: $tipoAmbiente")
+        println("Área Protegida: $areaProtegidaEmKm2 km²")
+        println("Possui Monitoramento: $possuiProgramaMonitoramento")
+    }
+
 
     //Isabel - Entender qual a função do constructor secundário:
-    constructor(tipoAmbiente: String): this(tipoAmbiente, 0.0, false){
-
-    }
+    constructor(tipoAmbiente: String): this(tipoAmbiente, 0.0, false){}
 }
 
 fun main() {
     //Isabel - Realizar os inputs da tarefa
     val conservacao: ConservacaoMarinha = ConservacaoMarinha("Coral",100.0, true)
-    conservacao.ambiente = "Manguezal"
-
+    conservacao.parqueNacional
+    conservacao.parqueNacional = "Manguezal"
 }
 
 
