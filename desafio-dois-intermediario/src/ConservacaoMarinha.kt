f632bec3aaf7898a2cd9543e7e071d419a6a5e2f
marevandro <marevandro95@gmail.com>
2024-08-31 16:57:25 -0300

    Desafio dois, nível intermediário. Iniciado

--- desafio-dois-intermediario/src/ConservacaoMarinha.kt
+++
@@ -1,9 +0,0 @@
-class ConservacaoMarinha(
-    val tipoAmbiente: TipoAmbienteMarinho,
-    val areaProtegidaEmKm2: Double,
-    val possuiProgramaMonitoramento: Boolean,
-) {
-    enum class TipoAmbienteMarinho{
-
-    }
-}
--- desafio-dois-intermediario/src/Main.kt
+++ desafio-dois-intermediario/src/Main.kt
@@ -1,3 +1,82 @@
-fun main(){
-    println("Hello World")
+import java.util.Scanner
+
+// Enumeração para os tipos de ambiente marinho
+enum class TipoAmbiente {
+    LITORANEA,
+    NERITICA,
+    ABATIAL,
+    ABISSAL
+}
+
+// Classe base para conservação marinha
+open class ConservacaoMarinha(
+    val tipoAmbiente: TipoAmbiente,
+    val areaProtegidaEmKm2: Double,
+    val possuirProgramaMonitoramento: Boolean
+) {
+    lateinit var detalhes: String
+
+    init {
+        detalhes = "Ambiente: $tipoAmbiente, " +
+                "Área protegida: $areaProtegidaEmKm2 km², " +
+                "Programa de monitoramento: ${if (possuirProgramaMonitoramento) "Sim" else "Não"}"
+    }
+
+    open fun mostrarDetalhes() {
+        println(detalhes)
+    }
+}
+
+// Subclasse para Reserva Marinha
+class ReservaMarinha(
+    tipoAmbiente: TipoAmbiente,
+    areaProtegidaEmKm2: Double,
+    possuirProgramaMonitoramento: Boolean
+) : ConservacaoMarinha(tipoAmbiente, areaProtegidaEmKm2, possuirProgramaMonitoramento) {
+
+    // lateinit para a variável tipoReserva
+    lateinit var tipoReserva: String
+
+    fun definirTipoReserva(opcao: Int) {
+        tipoReserva = when (opcao) {
+            1 -> "Estações Ecológicas"
+            2 -> "Reservas Biológicas"
+            3 -> "Parques Nacionais"
+            4 -> "Monumentos Naturais"
+            5 -> "Refúgios da Vida Silvestre"
+            else -> "Opção inválida"
+        }
+    }
+
+    override fun mostrarDetalhes() {
+        with(detalhes) {
+            println("$this, Tipo de Reserva: $tipoReserva")
+        }
+    }
+}
+
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Qual o tipo da reserva marinha?")
+
+    println("1. Estações Ecológicas")
+    println("2. Reservas Biológicas")
+    println("3. Parques Nacionais")
+    println("4. Monumentos Naturais")
+    println("5. Refúgios da Vida Silvestre")
+    print("Escolha uma das opções: ")
+
+    val opcao = scanner.nextInt()
+
+    val reservaMarinha = ReservaMarinha(
+        TipoAmbiente.LITORANEA, 500.0, true
+    )
+
+    // Define o tipo de reserva com base na escolha do usuário
+    reservaMarinha.definirTipoReserva(opcao)
+
+    // Exibe os detalhes da reserva marinha
+    reservaMarinha.mostrarDetalhes()
 }
